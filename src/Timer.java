6ed40ae21d7959479075713b9c915e190792ad6e
dkhvoya <d.khvoya@gmail.com>
2016-09-17 01:49:13 +0300

    Add files via upload

---
+++ src/Timer.java
@@ -0,0 +1,32 @@
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Timer extends Thread {
+	
+    private long endTime;
+    private long currentTime;
+    private long timeShowRate;
+	
+    public Timer(long waitTime, long timeShowRate) {
+    	this.timeShowRate = timeShowRate;
+    	currentTime = new Date().getTime();
+    	endTime = currentTime + waitTime;
+    }
+    
+    public void run() {
+    	long diff;
+		try {
+            while (true) {
+              Thread.sleep(timeShowRate);
+              currentTime += timeShowRate;
+              diff = endTime - currentTime;
+              long minLeft = TimeUnit.MILLISECONDS.toMinutes(diff);
+              long secLeft = diff / 1000 - minLeft * 60;
+              System.out.println("Time left: " + minLeft + " minutes " + secLeft + " seconds. Wait...");
+            }
+		} 
+		catch (InterruptedException e) {
+			
+		}
+    }
+}
