9114d6e62d5e06d640948878ee93424c9523d195
dkhvoya <d.khvoya@gmail.com>
2016-09-17 01:40:20 +0300

    Create BeepNotificator.java

---
+++ src/BeepNotificator.java
@@ -0,0 +1,16 @@
+import java.awt.Toolkit;
+
+public class BeepNotificator extends Thread {
+	
+    public void run() {   	
+		   try {
+         while (true) {
+			   Toolkit.getDefaultToolkit().beep();
+			   Thread.sleep(400);
+         }
+		   } 
+		   catch (InterruptedException e) {
+			     System.out.println("Temperature value has been recorded.");
+		   }
+    }
+}
