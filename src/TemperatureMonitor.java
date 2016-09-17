fc1db5205afe97e5e9d7845e836615d481d061fb
dkhvoya <d.khvoya@gmail.com>
2016-09-17 11:16:08 +0300

    Update TemperatureMonitor.java

--- src/TemperatureMonitor.java
+++ src/TemperatureMonitor.java
@@ -1,15 +1,10 @@
-import java.io.*;
-import java.nio.charset.Charset;
-import java.nio.file.*;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Scanner;
 
 public class TemperatureMonitor {
 
 	public static void main(String[] args) throws Exception {
 	    new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+	    args = prepareArgs(args);
 	    try {
                long waitTime = Integer.parseInt(args[0]) * 60000;
                long timeShowRate = Integer.parseInt(args[1]) * 1000;
@@ -21,7 +16,7 @@ public class TemperatureMonitor {
 	       BeepNotificator beepNotificator = new BeepNotificator();
 	       beepNotificator.start();
 	       String filePath = System.getProperty("user.home") + "/Desktop\\Temperature.txt";
-               recordTemperature(filePath);	    
+               new Serializer(filePath).recordTemperature();	    
 	       beepNotificator.interrupt();
 	       suggestStatistics(filePath);
 	    }
@@ -29,69 +24,21 @@ public class TemperatureMonitor {
 		System.out.println("Illegal arguments passed. Arguments represent wait time and show time rate and must be two integer numbers! Terminating...");
 	    }
 	}
-	private static boolean isValidTemperature(float temp) {
-	    if (temp >= 30.0 && temp <= 45.0)
-		return true;
-	    return false;
-	}
-	private static void recordTemperature(String filePath) {
-	    @SuppressWarnings("resource")
-	    Scanner sc = new Scanner(System.in);
-	    float temperature;
-	    while(true) {
-	    	try {
-	    	   if (isValidTemperature(temperature = Float.parseFloat(sc.nextLine()))) {
-	    	       DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-	    	       File f = new File(filePath);
-	    	       f.createNewFile();
-	    	       Files.write(Paths.get(filePath), (dateFormat.format(new Date()) + " — " + temperature + "\r\n===========================================\r\n").getBytes(), StandardOpenOption.APPEND);
-	    	       break;
-	           }
-	    	   throw new Exception();
-	        }
-	    	catch (Exception e) {
-	    	    System.out.println("Wrong temperature format!!! Enter a floating-point number.");
-	    	    continue;
-	    	}
+	private static String[] prepareArgs(String[] args) {
+	    if (args.length == 0) {
+		System.out.println("No arguments passed. Default values of 10 minutes and 30 seconds will be used.");
+		args = new String[2];
+		args[0] = "10";
+		args[1] = "30";
 	    }
-	}
-	private static float[] getAverages(BufferedReader br) {
-	    String line;
-	    int count = 0;
-	    float[] results = new float[9];
-            try {
-	      while ((line = br.readLine()) != null) {
-		  if (++count % 2 == 0) continue;
-		  int hours = Integer.parseInt(line.substring(11, 13));
-		  float temp = Float.parseFloat(line.substring(22));
-                  if (isBetween(hours, 0, 6)) {
-            	      results[7]++;
-            	      results[3] += temp;
-                  }
-                  else if (isBetween(hours, 7, 12)) {
-            	      results[4]++;
-            	      results[0] += temp;
-                  }
-                  else if (isBetween(hours, 13, 15)) {
-            	      results[5]++;
-            	      results[1] += temp;
-                  }
-                  else if (isBetween(hours, 16, 23)) {
-            	      results[6]++;
-            	      results[2] += temp;
-                  }
-	      }
-	      results[8] = (results[0] + results[1] + results[2] + results[3]) / (results[4] + results[5] + results[6] + results[7]);
-            }
-	    catch (Exception e) {
-		System.out.println("Error while reading from file. Terminating...");
-            }
-	    return results;
-	}
-	private static boolean isBetween(int num, int lowBound, int upBound) {
-	    if (num >= lowBound && num <= upBound) 
-		return true;
-	    return false;
+	    else if (args.length == 1) {
+		System.out.println("Only one argument passed. Default value of time show rate, 30 seconds, will be used.");
+		String temp = args[0];
+		args = new String[2];
+		args[0] = temp;
+		args[1] = "30";
+	    }
+	    return args;
 	}
 	private static void suggestStatistics(String filePath) {
 	    @SuppressWarnings("resource")
@@ -110,25 +57,5 @@ public class TemperatureMonitor {
 	        }
 		else System.out.print("Wrong input. Please, enter either \"Y\" or \"N\" character: ");
 	    }
-	}
-	private static void showAverageTemp(String filePath) {
-	    try {
-	      InputStream fis = new FileInputStream(filePath);
-	      InputStreamReader isr = new InputStreamReader(fis, Charset.forName("UTF-8"));
-	      BufferedReader br = new BufferedReader(isr);
-              float[] values = getAverages(br);
-	      if (values[4] != 0)
-		  System.out.println("Average morning (7 a.m. - 12 p.m.) temperature: " + values[0] / values[4]);
-	      if (values[5] != 0)
-		  System.out.println("Average afternoon (13 p.m. - 15 p.m.) temperature: " + values[1] / values[5]);
-	      if (values[6] != 0)
-		  System.out.println("Average evening (16 p.m. - 23 p.m.) temperature: " + values[2] / values[6]);
-	      if (values[7] != 0)
-		  System.out.println("Average night (0 a.m. - 6 a.m.) temperature: " + values[3] / values[7]);
-	      System.out.println("Total average temperature: " + values[8] + "\nTerminating...");
-	    }
-	    catch (Exception e) {
-		System.out.println("Error while reading from file.");
-		}
 	}
 }
